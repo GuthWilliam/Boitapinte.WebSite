5841daf16c0963e90b5db73d2b6cbd588bb70496
William GUTH <william.guth@viacesi.fr>
2022-04-13 23:37:00 +0200

    update htaccess

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { ContactComponent } from './contact/component/contact.component';
-import { DemoComponent } from './demo/demo.component';
 import { HomeComponent } from './home/home.component';
 import { TeamComponent } from './team/components/team/team.component';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'team', component: TeamComponent },
-    { path: 'demo', component: DemoComponent },
     { path: 'contact', component: ContactComponent }
   ];
